829338f40f53c94315e6688e5f46edc850c0c16e
Anishcharith <anish.charith@gmail.com>
2017-07-05 07:53:57 +0530

    creating directory if doesnt exist

--- getdata.py
+++ getdata.py
@@ -1,9 +1,10 @@
 import urllib.request
 import pandas as pd
+import os
 
 NIFTY=pd.read_csv('nifty.csv')
 NIFTYnse=NIFTY['Symbol'].values
-
+os.system('mkdir -p data/')
 for comp in NIFTYnse:
     urllib.request.urlretrieve('https://www.quandl.com/api/v3/datasets/NSE/'+comp+'.csv',filename='data/'+comp+'.csv')
     print(comp+' done')
