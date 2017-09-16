eaece002af898c3e85a4e4512d5bf0df253c2e43
Anishcharith <anish.charith@gmail.com>
2017-09-16 22:18:38 +0530

    made functions for repetetive stuff

--- adxvsoffset.py
+++ adxvsoffset.py
@@ -5,66 +5,54 @@ import adxmodel
 import matplotlib.pyplot as plt
 import adxvsoffsetclassifier as clf
 
+def plotpoints(p,n,color):
+    x=[]
+    y=[]
+    plt.scatter(p[0],p[1],color=color,marker='x')
+    plt.scatter(n[0],n[1],color=color,marker='o')
+    x.append([p[0],p[1]])
+    x.append([n[0],n[1]])
+    y.append(1)
+    y.append(0)
+    return x,y
+
+def plotline(x,y,color,label):
+    xx=np.linspace(0,50)
+    w,I=clf.main(x,y)
+    a=-w[0]/w[1]
+    yy=a*xx - I[0]/w[1]
+    plt.plot(xx,yy,color=color,label=label)
+
+
 def main(comp):
     p=[]
     n=[]
-    xx=np.linspace(0,50)
-    X10=[]
-    X20=[]
-    X30=[]
-    X40=[]
-    Y10=[]
-    Y20=[]
-    Y30=[]
-    Y40=[]
+    X=[[],[],[],[]]
+    Y=[[],[],[],[]]
     for i in range(8,50):
         x,y,r=adxmodel.main(comp,i)
         p,n=clusterfinder.main(x,y,r)
-        if i<50 and i>40:
-            plt.scatter(p[0],p[1],color='blue',marker='x')
-            plt.scatter(n[0],n[1],color='blue',marker='o')
-            X40.append([p[0],p[1]])
-            X40.append([n[0],n[1]])
-            Y40.append(1)
-            Y40.append(0)
         if i<20:
-            plt.scatter(p[0],p[1],color='green',marker='x')
-            plt.scatter(n[0],n[1],color='green',marker='o')
-            X10.append([p[0],p[1]])
-            X10.append([n[0],n[1]])
-            Y10.append(1)
-            Y10.append(0)
+            tempX,tempY=plotpoints(p,n,'green')
+            X[0]+=tempX
+            Y[0]+=tempY
         elif i<30:
-            plt.scatter(p[0],p[1],color='purple',marker='x')
-            plt.scatter(n[0],n[1],color='purple',marker='o')
-            X20.append([p[0],p[1]])
-            X20.append([n[0],n[1]])
-            Y20.append(1)
-            Y20.append(0)
+            tempX,tempY=plotpoints(p,n,'purple')
+            X[1]+=tempX
+            Y[1]+=tempY
         elif i<40:
-            plt.scatter(p[0],p[1],color='red',marker='x')
-            plt.scatter(n[0],n[1],color='red',marker='o')
-            X30.append([p[0],p[1]])
-            X30.append([n[0],n[1]])
-            Y30.append(1)
-            Y30.append(0)
+            tempX,tempY=plotpoints(p,n,'red')
+            X[2]+=tempX
+            Y[2]+=tempY
+        elif i<50:
+            tempX,tempY=plotpoints(p,n,'blue')
+            X[3]+=tempX
+            Y[3]+=tempY
 
     plt.title(comp)
-    w,I=clf.main(X10,Y10)
-    a=-w[0]/w[1]
-    yy=a*xx - I[0]/w[1]
-    plt.plot(xx,yy,color='green',label='9-20')
-    w,I=clf.main(X20,Y20)
-    a=-w[0]/w[1]
-    yy=a*xx - I[0]/w[1]
-    plt.plot(xx,yy,color='purple',label='20-30')
-    w,I=clf.main(X30,Y30)
-    a=-w[0]/w[1]
-    yy=a*xx - I[0]/w[1]
-    plt.plot(xx,yy,color='red',label='30-40')
-    w,I=clf.main(X40,Y40)
-    a=-w[0]/w[1]
-    yy=a*xx - I[0]/w[1]
-    plt.plot(xx,yy,color='blue',label='40-50')
+    plotline(X[0],Y[0],'green','9-20')
+    plotline(X[1],Y[1],'purple','20-30')
+    plotline(X[2],Y[2],'red','30-40')
+    plotline(X[3],Y[3],'blue','40-50')
     plt.legend()
     plt.show()
