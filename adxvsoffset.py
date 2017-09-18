466efe724000c5678b6c9fca5d2a28498154984f
Anishcharith <anish.charith@gmail.com>
2017-09-18 08:30:37 +0530

    buggy code

--- adxvsoffset.py
+++ adxvsoffset.py
@@ -17,14 +17,17 @@ def plot_contours(ax,clf,xx,yy,**params):
     out=ax.contourf(xx,yy,Z,**params)
     return out
 
-def collectpoints(p,n,color):
-    x=[]
-    y=[]
-    x.append([p[0],p[1]])
-    x.append([n[0],n[1]])
-    y.append(1)
-    y.append(0)
-    return x,y
+def collectpoints(comp):
+    X=[]
+    Y=[]
+    for i in range((comp[1]-1)*10+1,comp[1]*10+1):
+        x,y,r=adxmodel.main(comp[0],i)
+        p,n=clusterfinder.main(x,y,r)
+        X.append([p[0],p[1]])
+        X.append([n[0],n[1]])
+        Y.append(1)
+        Y.append(0)
+    return X,Y
 
 def plotline(x,y,color,label):
     xx=np.linspace(0,50)
@@ -40,8 +43,8 @@ def trainmodel(models,X,y):
 def main(comp):
     p=[]
     n=[]
-    X=[[],[],[],[]]
-    Y=[[],[],[],[]]
+    X=[]
+    Y=[]
     trainedmodels=[]
     color=['green','purple','red','blue']
     C=1.0
@@ -49,45 +52,22 @@ def main(comp):
             svm.LinearSVC(C=C),
             svm.SVC(kernel='rbf',gamma=0.7,C=C),
             svm.SVC(kernel='poly',degree=3,C=C))
-    for i in range(8,50):
-        x,y,r=adxmodel.main(comp,i)
-        p,n=clusterfinder.main(x,y,r)
-        if i<20:
-            tempX,tempY=collectpoints(p,n,'green')
-            X[0]+=tempX
-            Y[0]+=tempY
-        elif i<30:
-            tempX,tempY=collectpoints(p,n,'purple')
-            X[1]+=tempX
-            Y[1]+=tempY
-        elif i<40:
-            tempX,tempY=collectpoints(p,n,'red')
-            X[2]+=tempX
-            Y[2]+=tempY
-        elif i<50:
-            tempX,tempY=collectpoints(p,n,'blue')
-            X[3]+=tempX
-            Y[3]+=tempY
-
-    for i in range(4):
-        tempModel=trainmodel(models,X[i],Y[i])
-        trainedmodels.append(tempModel)
+    X,Y=collectpoints(comp)
+    trainedmodels=trainmodel(models,X,Y)
     titles=('SVC with linear kernel',
             'LinearSVC (linear kernel)',
             'SVC with rbg kernel',
             'SVC with polynomial (degree 3) kernel')
-    for i in range(4): 
-        X0=np.array([j[0] for j in X[i]])
-        X1=np.array([j[1] for j in X[i]])
-        xx,yy=make_meshgrid(X0,X1)
-        fig, sub = plt.subplots(2, 2)
-        plt.subplots_adjust(wspace=0.4, hspace=0.4)
-        
-        for clf,title,ax in zip(trainedmodels[i],titles,sub.flatten()):
-            plot_contours(ax,clf,xx,yy,cmap=plt.cm.coolwarm, alpha=0.8)
-            ax.scatter(X0, X1, c=Y[i], cmap=plt.cm.coolwarm, s=20, edgecolors='k')
-            ax.set_xlabel('adx')
-            ax.set_ylabel('price')
-            ax.set_title(title)
-        plt.show()
+    X0=np.array([j[0] for j in X])
+    X1=np.array([j[1] for j in X])
+    xx,yy=make_meshgrid(X0,X1)
+    fig, sub = plt.subplots(2, 2)
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    for clf,title,ax in zip(trainedmodels,titles,sub.flatten()):
+        plot_contours(ax,clf,xx,yy,cmap=plt.cm.coolwarm, alpha=0.8)
+        ax.scatter(X0, X1, c=Y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+        ax.set_xlabel('adx')
+        ax.set_ylabel('price')
+        ax.set_title(title)
+    plt.show()
 
